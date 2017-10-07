878dbae8beb5bcb5a9f2c86faba2b0aba5307c9a
worganic <worganic@gmail.com>
2017-10-07 18:07:56 +0200

    mod 3

--- app.py
+++ app.py
@@ -86,6 +86,6 @@ def makeWebhookResult(req):
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    #print("Starting app on port %d" % port)
+    #print("Starting aa app on port %d" % port)
 
     app.run(debug=True, port=port, host='0.0.0.0')
